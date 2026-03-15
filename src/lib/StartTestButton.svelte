38f76aba08523a6aeebdd7d36b3a3303ce146da4
ORANGETAN3422 <orangtan3422@gmail.com>
2026-03-15 12:18:32 +1100

    wiped

--- src/lib/StartTestButton.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let testActive;
-  export let debouncing;
-</script>
-
-<h3>
-  <b>{testActive ? "" : (debouncing ? "Please wait before starting again..." : "Start spamming [Space] to begin")}</b>
-</h3>
